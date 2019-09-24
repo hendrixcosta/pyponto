d229c501d393b68097e3d174f3cfbe4c6075aba9
Hendrix Costa <hendrix.costa@abgf.gov.br>
2019-09-24 09:52:27 -0300

    [ADD] Script generate user admin

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 cd /pyponto/
 python manage.py migrate
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
 python manage.py runserver 0.0.0.0:8000
